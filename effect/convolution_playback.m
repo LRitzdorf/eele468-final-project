55aed931eb6b42ccd69ab59c850223288c3c7d7a
Lucas Ritzdorf <42657792+LRitzdorf@users.noreply.github.com>
2024-02-29 23:10:55 -0700

    effect(matlab): resample impulse to match input

--- effect/convolution_playback.m
+++ effect/convolution_playback.m
@@ -4,15 +4,6 @@
 
 sampledir = "samples";
 
-%% Select and load impulse
-
-impulsename = uigetfile('*.wav', "Select an Impulse Response", sampledir);
-if impulsename == 0
-    error("File does not exist, or selection cancelled. Please try again.")
-end
-impulse = audioread(fullfile(sampledir, impulsename));
-plot(impulse);
-
 %% Select and load input
 
 inputname = uigetfile('*.wav', "Select an Input Sample", sampledir);
@@ -22,8 +13,19 @@ end
 [input, fs] = audioread(fullfile(sampledir, inputname));
 plot(input);
 
+%% Select and load impulse
 
-%% Ensure both signals are in stereo
+impulsename = uigetfile('*.wav', "Select an Impulse Response", sampledir);
+if impulsename == 0
+    error("File does not exist, or selection cancelled. Please try again.")
+end
+[impulse, fs_imp] = audioread(fullfile(sampledir, impulsename));
+plot(impulse);
+
+%% Ensure both signals have matching sample rates and are in stereo
+
+% Resample the impulse so it matches the input's sample rate
+impulse = audioresample(impulse, InputRate=fs_imp, OutputRate=fs);
 % If either file has only one audio stream (i.e. mono audio), copy it to
 % create two equivalent stereo channels
 impulse = stereoify(impulse);
