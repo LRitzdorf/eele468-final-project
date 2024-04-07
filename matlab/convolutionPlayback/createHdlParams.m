7e266759ae9c2b30b7ac3db93fa330150109fea4
Lucas Ritzdorf <42657792+LRitzdorf@users.noreply.github.com>
2024-04-07 00:03:03 -0600

    model: calculate oversampling factor on save
    This process now pulls in compiled model settings (mainly the different
    clock rates), so it has to be run on save, after the model has been
    compiled.

--- matlab/convolutionPlayback/createHdlParams.m
+++ matlab/convolutionPlayback/createHdlParams.m
@@ -1,28 +1,11 @@
 % Set HDL-specific parameters for the associated Simulink model
 
-function hdlParams = createHdlParams(modelParams)
+function hdlParams = createHdlParams(~)
 % Input is the modelParams struct from createModelParams()
 % Output is the hdlParams struct containing HDL related information
 
 % Speed of FPGA clock that we will connect to in the FPGA fabric
 FPGA_clock_frequency = 98304000; % Hz
+hdlset_param(gcs, "TargetFrequency", FPGA_clock_frequency);
 
-% Compute the HDL Coder oversampling factor
-% - https://www.mathworks.com/help/hdlcoder/ug/oversampling-factor.html
-% - https://www.mathworks.com/help/hdlcoder/ug/generating-a-global-oversampling-clock.html
-oversampling = FPGA_clock_frequency/modelParams.audio.sampleFrequency;
-if mod(oversampling, 1) == 0  % check if it is an integer
-    hdlParams.clockOversamplingFactor = oversampling;
-else
-    % Note the Simulink Diagnostic Viewer doesn't provide the stack trace, so
-    % we need to provide the location where this error is coming from if there
-    % is an error.
-    st = dbstack;
-    disp(['Error in ' st.name]);
-    error('Error: The clock oversampling factor must be an integer.')
-end
-% Oversampling factor is listed in Model Settings (Ctrl+E), under:
-% HDL Code Generation/Global Settings/Clock settings.
-% NOTE: we can't use a variable name for this, so we have to explicitly set it
-% with hdlset_param().
-hdlset_param(gcs, 'Oversampling', hdlParams.clockOversamplingFactor)
+hdlParams = struct();
