b8fc3e9fdacb059738daaf5de456d8d60a3972cb
Lucas Ritzdorf <42657792+LRitzdorf@users.noreply.github.com>
2024-04-17 12:59:49 -0600

    model: use a more basic, visual test case (temporarily)

--- matlab/convolutionPlayback/createSimParams.m
+++ matlab/convolutionPlayback/createSimParams.m
@@ -3,10 +3,8 @@
 function simParams = createSimParams(modelParams)
 
 % Audio source file for simulation
-impulse = getAudio('../effect/inputs/yop.wav', ...
-    modelParams.audio.sampleFrequency, 'left', modelParams.audio.dataType);
-stream  = getAudio('../effect/inputs/Congo Drummer.wav', ...
-    modelParams.audio.sampleFrequency, 'left', modelParams.audio.dataType);
+impulse = fi([0.5, -1, 0.5]', modelParams.audio.dataType);
+stream  = fi([0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0]', modelParams.audio.dataType);
 simParams.audioIn = [impulse; stream];
 % Control signal for mode (recording vs streaming)
 simParams.modeControl = fi([ones(size(impulse)); zeros(size(stream))], modelParams.modeControl.dataType);
@@ -14,8 +12,8 @@ simParams.modeControl = fi([ones(size(impulse)); zeros(size(stream))], modelPara
 % Simulation Parameters
 simParams.verifySimulation = true;
 simParams.playOutput       = true;
-simParams.stopTime         = 5; % seconds
+simParams.stopTime         = 25/48000; % seconds
 
 % Model parameters for simulation
-wetDryMix = 0.5;
+wetDryMix = 1;
 simParams.wetDryMix = fi(wetDryMix, modelParams.wetDryMix.dataType);
