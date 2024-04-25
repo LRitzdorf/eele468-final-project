ee3b286204d5466e2b029239a5455eaa68190b37
Lucas Ritzdorf <42657792+LRitzdorf@users.noreply.github.com>
2024-04-25 02:21:44 -0600

    model: express fabric clock rate in MHz, not Hz
    I was only off by six orders of magnitude...

--- matlab/convolutionPlayback/createHdlParams.m
+++ matlab/convolutionPlayback/createHdlParams.m
@@ -5,10 +5,10 @@ function hdlParams = createHdlParams(~)
 % Output is the hdlParams struct containing HDL related information
 
 % Speed of FPGA clock that we will connect to in the FPGA fabric
-FPGA_clock_frequency = 98304000; % Hz
+FPGA_clock_frequency = 98.304; % MHz
 hdlset_param(gcs, "TargetFrequency", FPGA_clock_frequency);
 
-hdlParams.clockFrequency = FPGA_clock_frequency;
+hdlParams.clockFrequency = FPGA_clock_frequency*10^6;
 
 % Oversampling rate (assuming the fastest clock in the system is the specified fabric clock)
 hdlset_param(gcs, 'Oversampling', 1)
